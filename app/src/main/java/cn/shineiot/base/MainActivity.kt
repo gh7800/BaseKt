d51e32f6b0b2c14ff27fe62e75cd46462b222f35
wangshuai <bjwangs@163.com>
2021-07-05 18:36:48 +0800

    先发布tag再push

--- app/src/main/java/cn/shineiot/base/MainActivity.kt
+++ app/src/main/java/cn/shineiot/base/MainActivity.kt
@@ -3,7 +3,6 @@ package cn.shineiot.base
 import androidx.viewbinding.ViewBinding
 import cn.shineiot.base.databinding.ActivityMainBinding
 import cn.shineiot.base.mvp.BaseMVPActivity
-import cn.shineiot.base.mvp.BaseView
 
 class MainActivity : BaseMVPActivity<MainView,MainPresenter>() {
     lateinit var viewBinding : ActivityMainBinding
@@ -20,16 +19,5 @@ class MainActivity : BaseMVPActivity<MainView,MainPresenter>() {
     override fun initView() {
         viewBinding.textView.text = "Hello"
     }
-/*
-    override fun showLoading() {
 
-    }
-
-    override fun dismissLoading() {
-
-    }
-
-    override fun errorMsg(msg: String?) {
-
-    }*/
 }
