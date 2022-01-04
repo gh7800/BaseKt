ee64498482c1c649ba2a2b7938bb361996657500
wangshuai <bjwangs@163.com>
2022-01-04 12:00:19 +0800

    修复一些过时的bug

--- app/src/main/java/cn/shineiot/base/App.kt
+++ app/src/main/java/cn/shineiot/base/App.kt
@@ -1,6 +1,9 @@
 package cn.shineiot.base
 
+import android.annotation.SuppressLint
+
 class App : BaseApplication(){
+    @SuppressLint("MissingSuperCall")
     override fun onCreate() {
         super.onCreate()
     }
